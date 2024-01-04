9012257893ebcff075986d70a6e93466371a2153
letitia-marta <133118806+letitia-marta@users.noreply.github.com>
2024-01-04 19:26:20 +0200

    Update comanda.php

--- comanda.php
+++ comanda.php
@@ -3,6 +3,12 @@
     include('dbcon.php');
     require 'myFunctions.php';
 
+    require 'vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
     if (isset($_SESSION['auth']))
     {
         if (isset($_POST['order-btn']))
@@ -57,6 +63,34 @@
                     VALUES ('$id_comanda','$id_produs','$cantitate','$pret')";
                     $insert_items_query_run = mysqli_query($con, $insert_items_query);
                 }
+
+                $mailer = new PHPMailer(true);
+
+                try
+                {
+                    $mailer->isSMTP();
+                    $mailer->Host = 'smtp.gmail.com';
+                    $mailer->SMTPAuth = true;
+                    $mailer->Username = 'formularcontact1@gmail.com';
+                    $mailer->Password = 'aayg mocl ifyq bnsv';
+                    $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                    $mailer->Port = 587;
+
+                    $mailer->setFrom('formularcontact1@gmail.com', 'Lumea prin obiectiv');
+                    $mailer->addAddress($email, $nume);
+
+                    $mailer->isHTML(true);
+                    $mailer->Subject = "Detalii comanda $nr_expeditie";
+                    $mailer->Body = "Salut, $nume! \n\n   Comanda $nr_expeditie a fost plasata si va fi expediata in cel mai scurt timp!
+                                    \n\nTotal de plata: $total RON\n(plata se face prin ramburs la livrare)";
+
+                    $mailer->send();
+                }
+                catch (Exception $e)
+                {
+                    echo 'A intervenit o eroare: ', $mailer->ErrorInfo;
+                }
+
                 $_SESSION['message'] = "Comanda a fost plasata";
                 header('Location: final.php');
                 die();
