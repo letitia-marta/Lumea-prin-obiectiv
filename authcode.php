401436f17a09b50e19fe5875df93461b119589a0
letitia-marta <133118806+letitia-marta@users.noreply.github.com>
2024-01-04 19:15:46 +0200

    Update authcode.php

--- authcode.php
+++ authcode.php
@@ -3,12 +3,25 @@
     include('dbcon.php');
     ob_start();
 
+    require 'vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    function generateSecurityCode()
+    {
+        return mt_rand(100000, 999999); 
+    }
+
     if (isset($_POST['buton_inregistrare']))
     {
         $nume = mysqli_real_escape_string($con,$_POST['nume']);
         $email = mysqli_real_escape_string($con,$_POST['email']);
         $parola = mysqli_real_escape_string($con,$_POST['parola']);
         $confirmare = mysqli_real_escape_string($con,$_POST['confirmare']);
+
+        $security_code = generateSecurityCode();
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL))
         {
@@ -28,34 +41,37 @@
             {
                 if ($parola == $confirmare)
                 {
-                    $insert_query = "INSERT INTO utilizatori (nume, email, parola) VALUES ('$nume', '$email', '$parola')";
+                    $insert_query = "INSERT INTO utilizatori (nume, email, parola, cod) VALUES ('$nume', '$email', '$parola', '$security_code')";
                     $insert_query_run = mysqli_query($con, $insert_query);
 
                     if ($insert_query_run)
                     {
-                        /*$to = $email;
-                        $subject = 'Verificare adresa e-mail';
-                        $message = "Iti multumim ca te-ai inregistrat!\n\n"
-                            . "Contul tau a fost creat, te vei putea conecta dupa ce validezi adresa ta de e-mail.\n\n"
-                            . "------------------------\n"
-                            . "Utilizator: $nume\n"
-                            . "------------------------\n\n"
-                            . "Acceseaza link-ul de mai jos pentru a valida adresa de e-mail:\n"
-                            . "http://www.lumeaprinobiectiv.com/verify.php?email=$email";
+                        $mailer = new PHPMailer(true);
 
-                        $headers = 'From: letitia.iliescu@gmail.com' . "\r\n";
-                        if (mail($to, $subject, $message, $headers))
+                        try
                         {
-                            $_SESSION['message'] = "A fost trimis mail-ul de validare";
-                            header('Location: register.php');
+                            $mailer->isSMTP();
+                            $mailer->Host = 'smtp.gmail.com';
+                            $mailer->SMTPAuth = true;
+                            $mailer->Username = 'formularcontact1@gmail.com';
+                            $mailer->Password = 'aayg mocl ifyq bnsv';
+                            $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                            $mailer->Port = 587;
+
+                            $mailer->setFrom('formularcontact1@gmail.com', 'Lumea prin obiectiv');
+                            $mailer->addAddress($email, $nume);
+
+                            $mailer->isHTML(true);
+                            $mailer->Subject = 'Cod de securitate pentru inregistrare';
+                            $mailer->Body = "Salut, $nume! \n\nCodul tau de securitate este: $security_code";
+
+                            $mailer->send();
                         }
-                        else
+                        catch (Exception $e)
                         {
-                            $_SESSION['message'] = "A intervenit o eroare. Detalii: " . error_get_last()['message'];
-                            header('Location: register.php');
-                        }*/
-                        $_SESSION['message'] = "Te-ai inregistrat cu succes";
-                        header('Location: login.php');
+                            echo 'A intervenit o eroare: ', $mailer->ErrorInfo;
+                        }
+                        header('location: email_validation.php');
                     }
                     else
                     {
