035c2f9765c096cf2653ed8f1df475a11b8c0853
letitia-marta <133118806+letitia-marta@users.noreply.github.com>
2024-01-04 11:41:11 +0200

    Update myFunctions.php

--- myFunctions.php
+++ myFunctions.php
@@ -1,5 +1,4 @@
 <?php
-    //session_start();
     include('dbcon.php');
 
     function getAll ($table)
@@ -43,6 +42,23 @@
                     ORDER BY c.id DESC";
 
             return $query_run = mysqli_query($con, $query);
+        }
+        else
+        {
+            //$_SESSION['message'] = 'Conecteaza-te pentru a accesa cosul de cumparaturi';
+            header('Location: login.php');
+        }
+    }
+
+    function getOrders()
+    {
+        global $con;
+        if (isset($_SESSION['auth_user']['user_id']))
+        {
+            $userId = $_SESSION['auth_user']['user_id'];
+            $query = "SELECT id, nr_expeditie, nume, telefon, adresa, cod_postal, total, creat_la
+                    FROM comenzi WHERE user_id = '$userId' ORDER BY creat_la DESC";
+            return $query_run = mysqli_query($con, $query);
             /*if ($query_run)
             {
                 return $query_run;
@@ -57,6 +73,42 @@
             //$_SESSION['message'] = 'Conecteaza-te pentru a accesa cosul de cumparaturi';
             header('Location: login.php');
         }
+    }
+
+    function getProducts($id_comanda)
+    {
+        global $con;
+        $products = array();
+
+        if (isset($_SESSION['auth_user']['user_id']))
+        {
+            $query = "SELECT p.name AS nume, c.cantitate AS cantitate, p.price AS pret
+                    FROM comanda AS c JOIN products AS p ON c.id_produs = p.id
+                    WHERE c.id_comanda = '$id_comanda'";
+            $query_run = mysqli_query($con, $query);
+
+            if ($query_run)
+            {
+                while ($row = mysqli_fetch_assoc($query_run))
+                {
+                    $products[] = $row;
+                }
+            }
+        }
+        else
+        {
+            // $_SESSION['message'] = 'Conecteaza-te pentru a accesa cosul de cumparaturi';
+            header('Location: login.php');
+        }
+
+        return $products;
+    }
+
+    function getFeedback()
+    {
+        global $con;
+        $query = "SELECT f.nume, f.email, f.subiect, f.mesaj, u.role_as FROM feedback f JOIN utilizatori u ON f.email = u.email ORDER BY f.id DESC";
+        return $query_run = mysqli_query($con, $query);
     }
 
     function redirect ($url, $message)
